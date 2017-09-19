c6a9c87c928baed1923c3860a0b4e2f017906148
josdas <josdas@mail.ru>
2017-09-19 18:47:00 +0300

    added two mode

--- main.py
+++ main.py
@@ -3,14 +3,16 @@ from events import Events
 from gui import EvningWindow, MorningWindow
 from PyQt5.QtWidgets import QApplication
 import datetime
+from city_weather import get_weather
 
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     hour = datetime.datetime.now().hour.real
-    if hour > 3 and hour < 15:
-        events = Events(suffix='morning')
+    cur_wather = get_weather()
+    if 3 < hour < 15:
+        events = Events(suffix='_morning', other_data=cur_wather)
         gui = MorningWindow(events)
     else:
-        events = Events(suffix='evning')
+        events = Events(suffix='_evening', other_data=cur_wather)
         gui = EvningWindow(events)
     sys.exit(app.exec_())
